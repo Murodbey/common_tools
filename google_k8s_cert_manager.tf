d97c59ea4975f1661ec7d81f45e1263dd664c236
Murodbey <abdullayevmurod@gmail.com>
2020-03-09 20:24:53 -0500

    cert manager file added

---
+++ google_k8s_cert_manager.tf
@@ -0,0 +1,21 @@
+# Deploy cert manger
+resource "helm_release" "cert_manager" {
+  depends_on = [
+    "kubernetes_namespace.service_tools",
+    "kubernetes_service_account.tiller",
+    "kubernetes_secret.tiller",
+    "kubernetes_cluster_role_binding.tiller_cluster_rule",
+    "kubernetes_deployment.vault_deployment",
+    "kubernetes_service.vault_service",
+    "kubernetes_service.nexus_service",
+  ] # "helm_release.grafana",
+
+  name      = "cert-manager"
+  chart     = "jetstack/cert-manager"
+  namespace = "${var.deployment_environment}"
+
+  wait = true
+}
+
+## Code should run first bellow comamnd and then deploy the code
+# kubectl apply --validate=false  -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.12/deploy/manifests/00-crds.yaml
